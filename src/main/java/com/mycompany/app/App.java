8e7a24d9c7cc8d1c2f7661e918c9c8585215b137
e825440 <agrawal.prateek1@gmail.com>
2017-12-14 08:20:04 +0530

    Add param in app.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		Console c=System.console();
+		String n=c.readLine();
+		System.out.println("Welcome "+n);
     }
 }
